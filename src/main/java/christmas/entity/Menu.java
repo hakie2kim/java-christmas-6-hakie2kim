ec725209b2f246996e3784bb07c4685de3da1b87
unknown <hakie2kim@gmail.com>
2023-11-11 21:11:35 +0900

    feat(src/main/java/christmas): change the error message

--- src/main/java/christmas/entity/Menu.java
+++ src/main/java/christmas/entity/Menu.java
@@ -75,7 +75,7 @@ public enum Menu {
             }
         }
 
-        throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        throw new IllegalArgumentException("[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요.");
     }
 
     public String getType() {
