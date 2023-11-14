e126fc350c7259fcd70d534bdbdf96f4cb08ca66
unknown <hakie2kim@gmail.com>
2023-11-14 16:08:43 +0900

    refactor(src/main/java/christmas): remove redundant lines

--- src/main/java/christmas/view/Input.java
+++ src/main/java/christmas/view/Input.java
@@ -10,8 +10,6 @@ public class Input {
 
     public static String[] readOrder() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        return Console.readLine().trim().split(","); // PatternSyntaxException
+        return Console.readLine().trim().split(","); // PatternSyntaxException instanceof IllegalArgumentException
     }
-
-
 }
