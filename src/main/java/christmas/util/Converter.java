06e7c7eea1e0aa9864b9dfd5e4cbf9171d0bd618
unknown <hakie2kim@gmail.com>
2023-11-14 20:34:19 +0900

    refactor(src/main/java/christmas): add converting features to README.md and change the Exception to specific type

--- src/main/java/christmas/util/Converter.java
+++ src/main/java/christmas/util/Converter.java
@@ -6,8 +6,8 @@ public class Converter {
     public static int stringToInteger(String beforeConversion, String errorMessage) {
         try {
             return Integer.parseInt(beforeConversion);
-        } catch (IllegalArgumentException illegalArgumentException) {
-            throw new IllegalArgumentException(errorMessage);
+        } catch (NumberFormatException numberFormatException) {
+            throw new NumberFormatException(errorMessage);
         }
     }
 
