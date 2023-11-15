4a874e04c74432b39ac776872b524a2c0ca0e4aa
unknown <hakie2kim@gmail.com>
2023-11-15 13:11:57 +0900

    refactor(src/main/java/christmas): make last day of month as a constant

--- src/main/java/christmas/entity/Event/Event.java
+++ src/main/java/christmas/entity/Event/Event.java
@@ -4,15 +4,24 @@ import christmas.entity.Order;
 import christmas.util.Converter;
 
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
 
 public abstract class Event {
-    protected final String NAME;
     private final static int YEAR = 2023;
     public final static int MONTH = 12;
+    public final static int LAST_DAY_OF_MONTH;
     protected final int reservationDayOfWeek; // 월요일 1부터 일요일 7까지
+    protected final String NAME;
     protected final int date;
     protected final Order order;
     protected int discount;
+
+    static {
+        LAST_DAY_OF_MONTH = LocalDate.of(YEAR, MONTH, 1)
+                .with(TemporalAdjusters.lastDayOfMonth())
+                .getDayOfMonth();
+    }
 
     protected Event(String name, int date, Order order) {
         NAME = name;
@@ -27,7 +36,7 @@ public abstract class Event {
     }
 
     public static void verifyDate(int date) {
-        if (!(1 <= date && date <= 31)) {
+        if (!(1 <= date && date <= LAST_DAY_OF_MONTH)) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
